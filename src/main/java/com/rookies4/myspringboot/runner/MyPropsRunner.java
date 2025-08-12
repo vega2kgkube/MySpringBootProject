a414a5f7b7baf8272b3a1dca97f04ff09d8a098d
vega2kgkube <vega2kgkube@gmail.com>
2025-08-12 16:49:45 +0900

    runner update

--- src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
+++ src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -8,12 +8,15 @@ import org.springframework.stereotype.Component;
 public class MyPropsRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("VM Arguments = " + args.containsOption("foo"));
-        System.out.println("Program Arguments = " + args.containsOption("bar"));
+        System.out.println("VM Arguments = " + args.containsOption("foo")); //false
+        System.out.println("Program Arguments = " + args.containsOption("bar")); //true
         
+        //Program Argument의 모든 이름을 출력하기
         for(String argName: args.getOptionNames()){
             System.out.println("아규먼트 이름 = " + argName);
         }
+
+
 
     }
 }
