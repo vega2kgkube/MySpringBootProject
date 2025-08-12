372c8b0d3a0e04f75b15ac426fb57b581b8a7226
vega2kgkube <vega2kgkube@gmail.com>
2025-08-12 16:09:26 +0900

    Runner arguments

--- src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
+++ src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -10,6 +10,10 @@ public class MyPropsRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("VM Arguments = " + args.containsOption("foo"));
         System.out.println("Program Arguments = " + args.containsOption("bar"));
+        
+        for(String argName: args.getOptionNames()){
+            System.out.println("아규먼트 이름 = " + argName);
+        }
 
     }
 }
