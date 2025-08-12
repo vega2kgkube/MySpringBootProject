09eeaab69b8d2435506514e002bc268c7c467750
vega2kgkube <vega2kgkube@gmail.com>
2025-08-12 15:36:22 +0900

    Runner add

---
+++ src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -0,0 +1,13 @@
+package com.rookies4.myspringboot.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyPropsRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
