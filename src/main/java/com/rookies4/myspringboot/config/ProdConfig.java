84cf6895c1ed1952ad38325b0e10d09c3be4080f
vega2kgkube <vega2kgkube@gmail.com>
2025-08-13 11:01:03 +0900

    @Profile add

--- src/main/java/com/rookies4/myspringboot/config/ProdConfig.java
+++ src/main/java/com/rookies4/myspringboot/config/ProdConfig.java
@@ -3,7 +3,9 @@ package com.rookies4.myspringboot.config;
 import com.rookies4.myspringboot.config.vo.CustomVO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("prod")
 @Configuration
 public class ProdConfig {
     @Bean
