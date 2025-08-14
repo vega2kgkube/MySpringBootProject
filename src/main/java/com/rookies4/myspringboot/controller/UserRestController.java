cf2bc532b67481c8035f01c87a1267a0828adcd2
vega2kgkube <vega2kgkube@gmail.com>
2025-08-14 16:12:48 +0900

    UserRestController

--- src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -1,15 +1,19 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.repository.CustomerRepository;
 import com.rookies4.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserRestController {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     //Constructor Injection
-    public UserRestController(UserRepository userRepository) {
-        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
-        this.userRepository = userRepository;
-    }
+//    public UserRestController(UserRepository userRepository) {
+//        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
 }
