9481a9f1e3f1e86ba385ba8a5794efb2d15978d2
vega2kgkube <vega2kgkube@gmail.com>
2025-08-14 09:58:55 +0900

    Repo query method

--- src/main/java/com/rookies4/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/rookies4/myspringboot/repository/CustomerRepository.java
@@ -3,5 +3,12 @@ package com.rookies4.myspringboot.repository;
 import com.rookies4.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    //customerId로 조회하는 Query Method 선언하기
+    Optional<Customer> findByCustomerid(String customerId);
+    //customerName으로 조회하는 Query Method 선언하기
+    List<Customer> findByCustomerName(String customerName);
 }
