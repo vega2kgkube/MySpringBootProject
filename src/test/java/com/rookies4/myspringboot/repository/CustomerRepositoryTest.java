ea43ae07acaeeb646a61a742b15b469b2e1cc1d5
vega2kgkube <vega2kgkube@gmail.com>
2025-08-14 10:35:52 +0900

    CustomerRepositoryTest update

--- src/test/java/com/rookies4/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rookies4/myspringboot/repository/CustomerRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+//assertj 라이브러리의 Assertions 클래스
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CustomerRepositoryTest {
@@ -21,6 +23,8 @@ class CustomerRepositoryTest {
         //When (실행단계)
         Customer savedCustomer = customerRepository.save(customer);
         //Then (검증단계)
+        //등록된 Customer 엔티티객체가 Null이 아닌지를 검증하기
+        assertThat(savedCustomer).isNotNull();
     }
 
 }
