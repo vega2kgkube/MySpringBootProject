28db516f02e9b18de73821051c5e09774d4eec09
vega2kgkube <vega2kgkube@gmail.com>
2025-08-14 16:35:22 +0900

    UserRestController create

--- src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -1,13 +1,18 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.repository.CustomerRepository;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserRestController {
     private final UserRepository userRepository;
 
@@ -16,4 +21,10 @@ public class UserRestController {
 //        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
 //        this.userRepository = userRepository;
 //    }
+
+    @PostMapping
+    public UserEntity create(@RequestBody UserEntity user){
+        return userRepository.save(user);
+    }
+
 }
