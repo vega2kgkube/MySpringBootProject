0f3f498dcaf5787cda3ca6062865088134b560c1
vega2kgkube <vega2kgkube@gmail.com>
2025-08-14 10:30:08 +0900

    CustomerRepositoryTest update

--- src/test/java/com/rookies4/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rookies4/myspringboot/repository/CustomerRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.rookies4.myspringboot.repository;
 
+import com.rookies4.myspringboot.entity.Customer;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,5 +12,15 @@ class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Test
+    void testSaveCustomer() {
+        //Given (준비단계)
+        Customer customer = new Customer();
+        customer.setCustomerId("AC001");
+        customer.setCustomerName("스프링부트");
+        //When (실행단계)
+        Customer savedCustomer = customerRepository.save(customer);
+        //Then (검증단계)
+    }
 
 }
