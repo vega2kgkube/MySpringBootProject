45ac355ad5b99d16f35a43cbd2ae86dde3f0a53e
vega2kgkube <vega2kgkube@gmail.com>
2025-08-14 15:54:24 +0900

    User Entity update

--- src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
+++ src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
@@ -4,13 +4,16 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
-public class User {
+@DynamicUpdate
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
