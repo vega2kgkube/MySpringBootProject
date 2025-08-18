2a6300757a4b2de79b4d6784091217fbc7a8b9ce
vega2kgkube <vega2kgkube@gmail.com>
2025-08-18 17:05:12 +0900

    update controller method

--- src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,6 +19,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
+
+    @GetMapping("/users/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id,
+                                 Model model) {
+        UserEntity user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+    @PostMapping("/users/update/{id}")
+    public String updateUser(@PathVariable("id") long id,
+                             @Valid @ModelAttribute("user") UserEntity user,
+                             BindingResult result) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+        userRepository.save(user);
+        return "redirect:/users/index";
+    }
 
     @GetMapping("/users/signup")
     public String showSignUpForm(@ModelAttribute("user") UserEntity user) {
