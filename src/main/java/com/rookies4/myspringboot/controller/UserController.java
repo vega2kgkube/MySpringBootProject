88791bb44c4b942e674716db3e72d9faf355b492
vega2kgkube <vega2kgkube@gmail.com>
2025-08-18 16:29:19 +0900

    user insert

--- src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -2,11 +2,14 @@ package com.rookies4.myspringboot.controller;
 
 import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,6 +22,18 @@ public class UserController {
     @GetMapping("/users/signup")
     public String showSignUpForm(@ModelAttribute("user") UserEntity user) {
         return "add-user";
+    }
+
+    @PostMapping("/users/adduser")
+    public String addUser(@Valid @ModelAttribute("user") UserEntity user,
+                          BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+        //return "redirect:/index";
     }
 
     @GetMapping("/users/index")
