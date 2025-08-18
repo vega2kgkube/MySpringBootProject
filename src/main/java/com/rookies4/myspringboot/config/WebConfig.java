f30a6c00b3c571e99a2d5f7032da86463ed86a85
vega2kgkube <vega2kgkube@gmail.com>
2025-08-18 14:13:38 +0900

    UserController add

--- src/main/java/com/rookies4/myspringboot/config/WebConfig.java
+++ src/main/java/com/rookies4/myspringboot/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //web url의 경로 설정
         registry.addResourceHandler("/mobile/**")
                 //반드시 mobile 다음에 / 을 주어야 한다.
                 .addResourceLocations("classpath:/mobile/")
