88066941d7d03a0f71a21aa05310722b92a5945e
vega2kgkube <vega2kgkube@gmail.com>
2025-08-18 14:46:18 +0900

    UserController update

--- src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -1,15 +1,26 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        List<UserEntity> userEntityList = userRepository.findAll();
+        return new ModelAndView("index","users",userEntityList);
+    }
+
 
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
