a8614cc1f869256895c894c2a3d73045746c6aac
vega2kgkube <vega2kgkube@gmail.com>
2025-08-18 16:02:24 +0900

    signup method

--- src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -14,6 +15,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
+
+    @GetMapping("/users/signup")
+    public String showSignUpForm(@ModelAttribute("user") UserEntity user) {
+        return "add-user";
+    }
 
     @GetMapping("/users/index")
     public ModelAndView index() {
