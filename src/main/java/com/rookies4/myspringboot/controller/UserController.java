4b5f181cc20ac2351caa4f2947426141d105f90a
vega2kgkube <vega2kgkube@gmail.com>
2025-08-18 14:40:19 +0900

    UserController update

--- src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name", "스프링부트");
