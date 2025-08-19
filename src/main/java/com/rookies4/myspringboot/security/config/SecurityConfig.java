580209069cb6b4fac7c6a9e52ca444435151047d
vega2kgkube <vega2kgkube@gmail.com>
2025-08-19 14:34:30 +0900

    SecurityConfig filter add

--- src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
+++ src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.rookies4.myspringboot.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,11 +10,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
+    //패스워드 암호화
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -21,14 +26,27 @@ public class SecurityConfig {
     @Bean
     //authentication(인증)을 위한 User 생성 (관리자, 일반사용자)
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        //관리자 생성
         UserDetails admin = User.withUsername("adminboot")
                 .password(encoder.encode("pwd1"))
                 .roles("ADMIN")
                 .build();
+        //일반사용자 생성
         UserDetails user = User.withUsername("userboot")
                 .password(encoder.encode("pwd2"))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/users/welcome").permitAll()
+                            .requestMatchers("/api/users/**").authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
     }
 }
