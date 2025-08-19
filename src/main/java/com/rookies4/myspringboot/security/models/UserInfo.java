a795ed9cf2faf73dba3c9a0eb2328334d17529a0
vega2kgkube <vega2kgkube@gmail.com>
2025-08-19 15:51:55 +0900

    UserInfo entity add

---
+++ src/main/java/com/rookies4/myspringboot/security/models/UserInfo.java
@@ -0,0 +1,25 @@
+package com.rookies4.myspringboot.security.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+    private String roles;
+}
