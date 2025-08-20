e781d536a578106db046154e3de1bc2b02d71dbe
vega2kgkube <vega2kgkube@gmail.com>
2025-08-20 10:42:52 +0900

    UserService createUser

--- src/main/java/com/rookies4/myspringboot/service/UserService.java
+++ src/main/java/com/rookies4/myspringboot/service/UserService.java
@@ -1,11 +1,20 @@
 package com.rookies4.myspringboot.service;
 
+import com.rookies4.myspringboot.controller.dto.UserDTO;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+
+    //User 등록
+    @Transactional
+    public UserDTO.UserResponse createUser(UserDTO.UserCreateRequest request) {
+
+    }
 }
