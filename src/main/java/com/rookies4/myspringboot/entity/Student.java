06388a52e9cb374a84645ca953b58a152a268e51
vega2kgkube <vega2kgkube@gmail.com>
2025-08-20 16:10:29 +0900

    student entity ㅓjoin column

--- src/main/java/com/rookies4/myspringboot/entity/Student.java
+++ src/main/java/com/rookies4/myspringboot/entity/Student.java
@@ -14,14 +14,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Student {
-    
+    //PK (Sequential Value)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
     private Long id;
     
+    //이름
     @Column(nullable = false)
     private String name;
     
+    //학번
     @Column(nullable = false, unique = true)
     private String studentNumber;
     
--- src/main/java/com/rookies4/myspringboot/entity/StudentDetail.java
+++ src/main/java/com/rookies4/myspringboot/entity/StudentDetail.java
@@ -16,22 +16,29 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class StudentDetail {
-    
+    //PK (Sequential Value)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_detail_id")
     private Long id;
-    
+
+    //주소
     @Column(nullable = false)
     private String address;
-    
+
+    //전화번호
     @Column(nullable = false)
     private String phoneNumber;
-    
-    @Column
+
+    //이메일
+    @Column(unique = true, nullable = false)
     private String email;
-    
+
+    //생년월일
     @Column
     private LocalDate dateOfBirth;
-    
+
+    //@JoinColumn은 FK(외래키)에 해당하는 어노테이션
+    @JoinColumn(name = "student_id", unique = true)
     private Student student;
 }
