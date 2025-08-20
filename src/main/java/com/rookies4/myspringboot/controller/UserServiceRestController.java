ee567576fa7f13cb2f6a9711384818f99acf6015
vega2kgkube <vega2kgkube@gmail.com>
2025-08-20 11:08:27 +0900

    UserServiceRestController add

--- src/main/java/com/rookies4/myspringboot/controller/UserServiceRestController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserServiceRestController.java
@@ -1,7 +1,12 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.controller.dto.UserDTO;
 import com.rookies4.myspringboot.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserServiceRestController {
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDTO.UserResponse> create(
+            @Valid @RequestBody UserDTO.UserCreateRequest request) {
+        UserDTO.UserResponse createdUser = userService.createUser(request);
+        return ResponseEntity.ok(createdUser);
+    }
 }
