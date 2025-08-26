4b8a978690fcd986d08e597095fd888a6bbb5da9
vega2kgkube <vega2kgkube@gmail.com>
2025-08-26 17:20:19 +0900

    BusinErrorCode call

--- src/main/java/com/rookies4/myspringboot/service/StudentService.java
+++ src/main/java/com/rookies4/myspringboot/service/StudentService.java
@@ -4,6 +4,7 @@ import com.rookies4.myspringboot.controller.dto.StudentDTO;
 import com.rookies4.myspringboot.entity.Student;
 import com.rookies4.myspringboot.entity.StudentDetail;
 import com.rookies4.myspringboot.exception.BusinessException;
+import com.rookies4.myspringboot.exception.ErrorCode;
 import com.rookies4.myspringboot.repository.StudentDetailRepository;
 import com.rookies4.myspringboot.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
@@ -33,14 +34,13 @@ public class StudentService {
 
     public StudentDTO.Response getStudentById(Long id) {
         Student student = studentRepository.findByIdWithStudentDetail(id)
-                .orElseThrow(() -> new BusinessException("Student not found with id: " + id, HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new BusinessException(ErrorCode.RESOURCE_NOT_FOUND,"Student","Id",id));
         return StudentDTO.Response.fromEntity(student);
     }
 
     public StudentDTO.Response getStudentByStudentNumber(String studentNumber) {
         Student student = studentRepository.findByStudentNumber(studentNumber)
-                .orElseThrow(() -> new BusinessException("Student not found with student number: " + studentNumber,
-                        HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new BusinessException(ErrorCode.RESOURCE_NOT_FOUND,"Student","학번",studentNumber));
         return StudentDTO.Response.fromEntity(student);
     }
 
