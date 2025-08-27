d5359f6b8c2127c29f3f452a60900554edc73731
vega2kgkube <vega2kgkube@gmail.com>
2025-08-27 17:14:30 +0900

    DTO add

--- src/main/java/com/rookies4/myspringboot/repository/StudentRepository.java
+++ src/main/java/com/rookies4/myspringboot/repository/StudentRepository.java
@@ -22,7 +22,7 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     //Department 관련해서 새로 추가된 메서드
     @Query("SELECT s FROM Student s LEFT JOIN FETCH s.studentDetail LEFT JOIN FETCH s.department WHERE s.id = :id")
-    Optional<Student> findByIdWithAllDetails(@Param("id") Long id);
+    Optional<Student> findByIdWithAllDetails(@Param("id") Long studentId);
 
     // 새로 추가된 메서드
     List<Student> findByDepartmentId(Long departmentId);
