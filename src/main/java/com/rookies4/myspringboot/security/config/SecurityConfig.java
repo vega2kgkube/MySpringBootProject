f907f7fbacaa2fae31228739e3316540b8cc08bc
vega2kgkube <vega2kgkube@gmail.com>
2025-09-04 14:35:04 +0900

    SecurityConfig add

---
+++ src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.rookies4.myspringboot.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/userinfos/welcome").permitAll()
+                            .requestMatchers("/api/**").authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+
+
+}
