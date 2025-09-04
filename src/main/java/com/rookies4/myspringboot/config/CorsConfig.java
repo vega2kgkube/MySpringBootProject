0a65c1b16c3ede3ebd1508b75644c3fda6904638
vega2kgkube <vega2kgkube@gmail.com>
2025-09-04 13:43:11 +0900

    CorsConfig update

--- src/main/java/com/rookies4/myspringboot/config/CorsConfig.java
+++ src/main/java/com/rookies4/myspringboot/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.rookies4.myspringboot.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -12,24 +13,39 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig {
+//    @Bean
+//    public FilterRegistrationBean<?> corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOriginPatterns(List.of("*"));
+//        configuration.setAllowCredentials(true);
+//        configuration.setAllowedHeaders(Arrays.asList(
+//                "Access-Control-Allow-Headers",
+//                "Access-Control-Allow-Origin",
+//                "Access-Control-Request-Method",
+//                "Access-Control-Request-Headers",
+//                "Origin","Cache-Control", "Content-Type", "Authorization"));
+//        configuration.setAllowedMethods(Arrays.asList("POST", "DELETE", "GET", "PATCH", "PUT"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//
+//        FilterRegistrationBean<?> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+//        bean.setOrder(0);
+//        return bean;
+//    }
+
     @Bean
-    public FilterRegistrationBean<?> corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList(
-                "Access-Control-Allow-Headers",
-                "Access-Control-Allow-Origin",
-                "Access-Control-Request-Method",
-                "Access-Control-Request-Headers",
-                "Origin","Cache-Control", "Content-Type", "Authorization"));
-        configuration.setAllowedMethods(Arrays.asList("POST", "DELETE", "GET", "PATCH", "PUT"));
-
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
 
-        FilterRegistrationBean<?> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(0);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return bean;
     }
 }
