5326c6c8d0354c3e448a6e2fba55213bb27dd974
vega2kgkube <vega2kgkube@gmail.com>
2025-09-04 16:03:00 +0900

    UserInfoController login add

--- src/main/java/com/rookies4/myspringboot/security/controller/UserInfoController.java
+++ src/main/java/com/rookies4/myspringboot/security/controller/UserInfoController.java
@@ -1,8 +1,14 @@
 package com.rookies4.myspringboot.security.controller;
 
+import com.rookies4.myspringboot.security.controller.dto.AuthRequest;
+import com.rookies4.myspringboot.security.jwt.JwtService;
 import com.rookies4.myspringboot.security.models.UserInfo;
 import com.rookies4.myspringboot.security.models.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +19,11 @@ public class UserInfoController {
     private UserInfoRepository repository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -27,4 +38,17 @@ public class UserInfoController {
         return savedUserInfo.getName() + " user added!!";
     }
 
+    @PostMapping("/login")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),
+                        authRequest.getPassword()
+                ));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
 }
