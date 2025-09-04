bec1eb69c21c67a4de3ee09a55a624d581e494ce
vega2kgkube <vega2kgkube@gmail.com>
2025-09-04 16:21:09 +0900

    JwtAuthenticationFilter add

--- src/main/java/com/rookies4/myspringboot/security/controller/UserInfoController.java
+++ src/main/java/com/rookies4/myspringboot/security/controller/UserInfoController.java
@@ -40,12 +40,15 @@ public class UserInfoController {
 
     @PostMapping("/login")
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        //인증 과정 처리
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         authRequest.getEmail(),
                         authRequest.getPassword()
                 ));
+        //인증 성공여부 체크
         if (authentication.isAuthenticated()) {
+            //인증 성공하면 토큰 생성
             return jwtService.generateToken(authRequest.getEmail());
         } else {
             throw new UsernameNotFoundException("invalid user request !");
